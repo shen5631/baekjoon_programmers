7d80f0a834feeccc8ab6930e79dd381b13ff46e1
shen5631 <shen5631@naver.com>
2025-04-13 22:04:27 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+    class Main{
+        public static void main(String args[]){
+              Scanner scan = new Scanner(System.in);
+                    double A =scan.nextInt();
+                    double B =scan.nextInt();
+                    double c = A/B;
+                    System.out.print(c);
+        }
+    }
+            
