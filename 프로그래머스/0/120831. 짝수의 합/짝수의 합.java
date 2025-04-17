5d3d39522bb8350a5486b4c1e8d170a25aff6c8e
shen5631 <shen5631@naver.com>
2025-04-17 22:50:22 +0900

    [level 0] Title: 짝수의 합, Time: 1.94 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int a;
+        int b = 0;
+        int c= n/2;
+        int answer = 0;
+        for(a=0;a<=(c*c+c-1);a+=1);
+        b += a;
+        return a;
+    }
+}
