1c7f512731c9eba4ffd50e9fb6f95a48e183b27b
shen5631 <shen5631@naver.com>
2025-05-20 21:40:12 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/README.md
+++ 프로그래머스/0/181939. 더 크게 합치기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 더 크게 합치기 - 181939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939#) 
 
 ### 성능 요약
 
-메모리: 79 MB, 시간: 0.02 ms
+메모리: 76.4 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 20일 21:24:54
+2025년 05월 20일 21:40:10
 
 ### 문제 설명
 
--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,37 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        int c=0;
-        int d=0;
         int e=0;
         int f=0;
         
-        if(a/1000!=0){      //a 판별 1000자리 판별
-            c=a/1000;       //앞자리만 남기고 날리기
-        }
-        else if(a/100!=0){  //100자리 판별
-            c=a/100;        //앞자리만 남기고 날리기
-        }
-        else if(a/10!=0){   //10자리 판별
-            c=a/10;         //앞자리만 남기고 날리기
-        }
-        else{               //1자리 판별
-            c=a;            //앞자리만 남기고 날리기
-        }
         
-        
-        if(b/1000!=0){      //b판별
-            d=b/1000;       
-        }
-        else if(b/100!=0){
-            d=b/100;
-        }
-        else if(b/10!=0){
-            d=b/10;
-        }
-        else{
-            d=b;
-        }
             if(b/1000!=0){
                e = a*10000+b;
             }
