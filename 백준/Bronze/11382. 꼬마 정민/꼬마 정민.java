981669c03ff0895df0885431f0510c8555d3c1d7
shen5631 <shen5631@naver.com>
2025-05-20 11:34:30 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+    class Main{
+        public static void main(String args[]){
+            Scanner scan=new Scanner (System.in);
+            long a = scan.nextLong();
+            long b = scan.nextLong();
+            long c = scan.nextLong();
+        
+            System.out.print(a+b+c);
+    }
+}
