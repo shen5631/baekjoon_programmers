500a10404ca7492ea5ddd8da2954c41917f889b7
shen5631 <shen5631@naver.com>
2025-05-20 20:12:22 +0900

    [Bronze V] Title: A+B - 3, Time: 200 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        
+        int a=scan.nextInt();
+        
+        for(int i=1;i<=a;i++){
+            int b=scan.nextInt();
+            int c=scan.nextInt();
+            
+            System.out.println((c+b));
+        }
+    }
+}
