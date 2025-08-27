d7366d249c74dc9462d54104009f9e9d2ea71e1b
shen5631 <shen5631@naver.com>
2025-08-27 11:16:33 +0900

    [level 1] Title: 자릿수 더하기, Time: 7.93 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String a = String.valueOf(n);
+        String b [] = new String [a.length()];
+        
+        for(int i = 0 ; i < a.length() ; i++){
+            b[i]=a.charAt(i)+"";
+        }
+        for(int i = 0 ; i < b.length ; i++){
+            answer+=Integer.parseInt(b[i]);
+        }
+        return answer;
+    }
+}
