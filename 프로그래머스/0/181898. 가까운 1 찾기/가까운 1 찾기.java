8b6bbcb35203e36c12866c60364344c09504441d
shen5631 <shen5631@naver.com>
2025-12-16 09:09:15 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.64 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
+        for(int i = 0 ; i< idx ; i++){
+            arr[i] = 0;
+        }
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i]==1){
+                answer=i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
