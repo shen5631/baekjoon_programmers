f50760a90c26feabdfd133704908b4d76f1a98b7
shen5631 <shen5631@naver.com>
2026-03-17 18:21:47 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+        int sum [] = new int[score.length];
+        for (int i = 0 ; i < sum.length ; i++){
+            answer[i]=1;
+        }
+        
+        
+        for(int i = 0 ; i < score.length ; i++){
+            sum[i]+= score[i][0] + score[i][1];
+        }
+        
+        for(int i = 0 ; i < sum.length ; i++){
+            for(int j = 0 ; j < sum.length ; j++){
+                if(sum[i] < sum[j] && i != j){
+                    answer[i]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
