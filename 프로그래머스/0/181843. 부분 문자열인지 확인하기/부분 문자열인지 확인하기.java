71aa33aabbc66fa24b77feaa3f8d33fb3c9e2619
shen5631 <shen5631@naver.com>
2026-04-12 21:08:41 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        
+        
+                
+        if(my_string.indexOf(target) != -1){
+            answer = 1;
+        }
+        return answer;
+    }
+}
