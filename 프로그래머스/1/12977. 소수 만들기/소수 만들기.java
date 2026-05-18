478508f7792d7335caef04a8f5fd1c3eecfbdd6b
shen5631 <shen5631@naver.com>
2026-05-19 07:54:22 +0900

    [level 1] Title: 소수 만들기, Time: 48.15 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int cnt =0;
+        int sum = 0;
+        
+        for(int i = 0;   i < nums.length ; i++){
+            for(int j = i+1 ; j < nums.length ; j++){
+                for(int k = j+1 ; k < nums.length ; k++){
+                    sum=nums[i]+nums[j]+nums[k];
+                    for(int h = 1 ; h <= sum ; h++){
+                        if(sum%h == 0){
+                            cnt++;
+                        }
+                    }
+                    if(cnt == 2){
+                        answer++;
+                    }
+                    cnt=0;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
