e262065fb7b059048231b42321e5e7258f745525
Hongmyeong Shin <shen5631@naver.com>
2026-06-20 21:38:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.24 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap <String,Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < participant.length ; i++){
+            if(map.containsKey(participant[i])){
+                map.put(participant[i],map.get(participant[i])+1);
+            }
+            else{
+                map.put(participant[i],1);
+            }
+        }
+        
+        for(int i = 0 ; i < completion.length ; i++){
+            if(map.containsKey(completion[i])){
+                map.put(completion[i],map.get(completion[i])+1);
+            }
+            else{
+                map.put(completion[i],1);
+            }
+        }
+        
+        for(String str : map.keySet()){
+            if(map.get(str) % 2 == 1){
+                return str;
+            }
+        }
+        
+        System.out.println(map);
+        
+        return answer;
+    }
+}
